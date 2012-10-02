7bedfcf0591ca3ae95f0672bceb89c7bda6571b9
Morten <Quizmoller@gmail.com>
2012-10-02 10:22:39 +0200

    Client Update
    Client update - Works with Random.uniform and makes funny graph's

--- src/Client_SCANeR.py
+++ src/Client_SCANeR.py
@@ -7,17 +7,18 @@ Created on 26/09/2012
  
 import socket               
 import time
-i=1                                 #variable i = 1
+import random
+i=random.uniform                    # variable i = Random number or hole number
 s = socket.socket()                 # Create Socket   
 host = socket.gethostname()         # Host IP   
-port = 12345                        #Host Port   
+port = 12345                        # Host Port   
  
-print 'Connecting to ', host, port  #Bind to the port
-s.connect((host, port))             #Connect to Host IT and port 
+print 'Connecting to ', host, port  # Bind to the port
+s.connect((host, port))             # Connect to Host IT and port 
 
 while True:                         # While Loop
-    i=1+i                           # add 1 for every time 
-    msg = '%d'%i                    # Message 
+    i=random.uniform (1,5)          # Random a number between 1 and 5 /  add 1 for every time 
+    msg = '%d'%i                    # Message to Server  
     s.sendall(msg)                  # Send message to all
     #print 1+1
     msg = s.recv(1024)              # Message Recv  
