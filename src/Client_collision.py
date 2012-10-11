ada7cb64e3ab8828d4373f2035050e08bbbb6c57
Morten <Quizmoller@gmail.com>
2012-10-11 10:29:57 +0200

    Small Update
    A small update. Adding more comments to each line

--- src/Client_collision.py
+++ src/Client_collision.py
@@ -8,26 +8,26 @@ Created on 26/09/2012
 import socket               
 #import time
 #import random
-#i=random.uniform                        # variable i = Random number or hole number
-def main():                                #def main(): is for SCANeR to run the script correctly (and multiple times)
-    s = socket.socket()                 # Create Socket   
-    host = "10.115.254.55"              # Host IP   
-    port = 12345                        # Host Port   
+#i=random.uniform                            # variable i = Random number or hole number
+def main():                                  #def main(): is for SCANeR to run the script correctly (and multiple times)
+    s = socket.socket()                      # Create Socket   
+    host = "10.115.254.55"                   # Host IP   
+    port = 12345                             # Host Port   
  
     print 'CollisionClient is connecting to ', host, port  # Bind to the port
-    s.connect((host, port))             # Connect to Host IT and port
+    s.connect((host, port))                  # Connect to Host IT and port
     print 'CollisionClient is connected' 
 
-    #while True:                         # While Loop
-    #    i=random.uniform (1,5)          # Random a number between 1 and 5 /  add 1 for every time 
-    #    msg = '%d'%i                    # Message to Server  
-    msg ="Collision"                    # Message is the name of the rule in SCANeR
-    s.sendall(msg)                      # Send message to all
+    #while True:                             # While Loop
+    #    i=random.uniform (1,5)              # Random a number between 1 and 5 /  add 1 for every time 
+    #    msg = '%d'%i                        # Message to Server  
+    msg ="Collision"                         # Message is the name of the rule in SCANeR
+    s.sendall(msg)                           # Send message to all
     #print 1+1
-    msg = s.recv(1024)                  # Message Recv  
+    msg = s.recv(1024)                       # Message Recv  
     print 'CollisionClient is receiving: ', msg    #Print to Screen msg is the total number of collisions received from the server
-#    print 'Math doing OK', msg          # Print to Screen 
-#    time.sleep(1)                       # Sleep timer in seconds 
+#    print 'Math doing OK', msg              # Print to Screen 
+#    time.sleep(1)                           # Sleep timer in seconds 
 #s.close ("exit")
     s.close()
-    return 0                            #end of def main():
+    return 0                                #end of def main():
--- src/Server_SCANeR.py
+++ src/Server_SCANeR.py
@@ -8,14 +8,14 @@ Created on 09/10/2012
 #import pylab                        # Import pylab      
 import socket                       # Import socket module
 
-oor = 0.0
+oor = 0.0                           
 coll = 0.0
-#q = socket.socket()                 # Create a socket object for Quit
+#q = socket.socket()                # Create a socket object for Quit
 s = socket.socket()                 # Create a socket object for Start 
                 
 host = socket.gethostname()         # Get local machine name
 port = 12345 
-portq = 12346
+#portq = 12346
 print 'Server started!'
 print 'Waiting for clients...'
 
@@ -24,27 +24,27 @@ s.bind((host, port))                    # Bind to the port
 #q.bind((host, portq))                   # Bind to the port
 #q.listen(5)
 #h,addr2 = q.accept()
-while oor < 5 and coll < 4 :            # While loop for Coll and oor 
+while oor < 5 and coll < 4 :            # While loop for Coll and oor for a <  
     s.listen(5)                         # Now wait for client connection.
     c, addr = s.accept()                # Establish connection with client.
     print 'Got connection from', addr   # Print Connetion and address
     msg = c.recv(1024)                  # msg receve from client
-    if msg == 'OutOfRoad'  : 
-        oor = oor + 1
+    if msg == 'OutOfRoad'  :            # if Massage = Out of Road go into if statement 
+        oor = oor + 1                   # Count oor + 1 
         print (oor), ' >> ', msg       # Print address and msg from Client
         #c, addr = s.accept()
         
-    elif msg == 'Collision' :
-        coll = coll + 1
+    elif msg == 'Collision' :           #els if msg = Collision go into Statement as above
+        coll = coll + 1                 # Count Coll + 1 
         print (coll), ' >> ', msg       # Print address and msg from Client
         #c, addr = s.accept()
     
-    if oor < 5 and coll < 4 :    
+    if oor < 5 and coll < 4 :           # If oor < 5 and coll < 4 
         c.send(msg);                    # Send msg to client
-    else :
-        c.send("Exit")
-    c.close()
-    print oor , "Udenfor "
-    print coll , "Sammen" 
+    else :                              # Else
+        c.send("Exit")                  # send Exit to Client 
+    c.close()                           # Close Connection
+    print oor , "Udenfor "              # Print oor as "Udenfor" Togeather  
+    print coll , "Sammen"               # Print coll as "Sammen" Together 
 #h.send("exit");                     # send exit message to Client 
 #h.close()                           # Close     Connection to client
